583e0550ad914d791360161210e0ac558e605e45
Rishabh Chauhan <dr.smolderrishabhchauhan@gmail.com>
2025-05-14 12:47:38 +0530

    Graphs surrounded by fragment tag

--- expense-tracker-frontend/src/Pages/HomePage.jsx
+++ expense-tracker-frontend/src/Pages/HomePage.jsx
@@ -130,64 +130,68 @@ const HomePage = () => {
         </div>
       )}
       {expenses.length === 0 && <div>No expenses yet...</div>}
-      {expenses.length > 0 && (<div className="flex justify-center items-center py-5 ">
-        <div className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-lg">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-red-400 pt-2 px-2">
-            &#8377; {totalExpenses}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400 pb-2 px-2">
-            Total Expense
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-center items-center w-full h-full gap-30 py-3">
-        <div>
-          <LineChart
-            width={800}
-            height={500}
-            data={dataForLineChart}
-            margin={{ bottom: 20, left: 20, right: 5, top: 5 }}
-          >
-            <Line type="monotone" dataKey="Amount" stroke="#ffa600" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="Date">
-              <Label
-                value="Dates on which expenses were made"
-                offset={0}
-                position="bottom"
-              />
-            </XAxis>
-            <YAxis>
-              <Label
-                value="Amount spent"
-                offset={0}
-                position="left"
-                angle="-90"
-                textAnchor="middle"
-              />
-            </YAxis>
-            <Tooltip content={<CustomTooltip />} />
-          </LineChart>
-        </div>
-        <div>
-          <PieChart width={500} height={500}>
-            <Pie
-              data={dataForPieChart}
-              dataKey="Amount"
-              nameKey="Category"
-              cx="50%"
-              cy="50%"
-              outerRadius={170}
-            >
-              {dataForPieChart.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={colors[index]} />
-              ))}
-              <LabelList dataKey="Category" position="outside" />
-            </Pie>
-            <Tooltip content={<CustomTooltip chart="pie" />} />
-          </PieChart>
-        </div>
-      </div>)}
+      {expenses.length > 0 && (
+        <>
+          <div className="flex justify-center items-center py-5 ">
+            <div className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-lg">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-red-400 pt-2 px-2">
+                &#8377; {totalExpenses}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400 pb-2 px-2">
+                Total Expense
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center w-full h-full gap-30 py-3">
+            <div>
+              <LineChart
+                width={800}
+                height={500}
+                data={dataForLineChart}
+                margin={{ bottom: 20, left: 20, right: 5, top: 5 }}
+              >
+                <Line type="monotone" dataKey="Amount" stroke="#ffa600" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="Date">
+                  <Label
+                    value="Dates on which expenses were made"
+                    offset={0}
+                    position="bottom"
+                  />
+                </XAxis>
+                <YAxis>
+                  <Label
+                    value="Amount spent"
+                    offset={0}
+                    position="left"
+                    angle="-90"
+                    textAnchor="middle"
+                  />
+                </YAxis>
+                <Tooltip content={<CustomTooltip />} />
+              </LineChart>
+            </div>
+            <div>
+              <PieChart width={500} height={500}>
+                <Pie
+                  data={dataForPieChart}
+                  dataKey="Amount"
+                  nameKey="Category"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={170}
+                >
+                  {dataForPieChart.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index]} />
+                  ))}
+                  <LabelList dataKey="Category" position="outside" />
+                </Pie>
+                <Tooltip content={<CustomTooltip chart="pie" />} />
+              </PieChart>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
